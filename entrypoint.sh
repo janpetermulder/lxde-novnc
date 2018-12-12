e086a80497568277560e65b7cd27d0abae388dea
janpetermulder <36788745+janpetermulder@users.noreply.github.com>
2018-12-12 13:22:53 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 chown -R $USER:$USER /root/.vnc
 chmod u=rw,g=,o= /root/.vnc/passwd
 
-/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+/usr/bin/supervisord.sh -c /etc/supervisor/conf.d/supervisord.conf
 exec bash -c "vncserver :1 -geometry $RESOLUTION -depth 24 && tail -F /root/.vnc/*.log"
 
 exec "$@"
